2db171c7a24c26c0042aa2391527db6fcc49eb00
jojoalva <140528886+jojoalva@users.noreply.github.com>
2023-08-24 23:50:26 +0100

    last version

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,25 +10,27 @@ with open (election_data, encoding= 'UTF-8') as csvfile:
     data_reader = csv.DictReader(csvfile)    
     print (data_reader)
 
-    #read header in data
-    csv_header = next(data_reader)
-    print (f"(CSV Header: + {csv_header})")
+    #voter ID rows = total number of votes
+    #voted_id = 0 because in a dictionary, the first row along with the header has values so not to ignore
+    voter_id=0
 
-    #total voters = total number of rows
-    voter_id=1
+    #set empty list to append unique names into later
     unique_candidate_list=[]
+    #set candidate_counts as empty dictionary to count total number of votes per candidate later
     candidate_counts = {}
 
-    
+    #loop through each row of voter id, and append unique names into list as above
     for row in data_reader:
         voter_id+=1
         target_column = row['Candidate']
-
+                      
         if  row['Candidate'] not in unique_candidate_list:
-            unique_candidate_list.append(row['Candidate'])
-            
+            unique_candidate_list.append(target_column)
+
+       #if current candidate name is inside dictionary, add 1 to the count. 
         if target_column in candidate_counts:
             candidate_counts[target_column] += 1
+        #Else, set value to 1 and move to next iteration
         else:
             candidate_counts[target_column] = 1
  
@@ -38,29 +40,25 @@ with open (election_data, encoding= 'UTF-8') as csvfile:
     print(f"Total votes per candidate: {candidate_counts}")
 
     winner = max(candidate_counts, key=candidate_counts.get)
-
-    for candidate, count in candidate_counts.items():
-        percentage = round((count / voter_id)*100 , 2)
-        print({candidate}: {percentage}%)
-
-    print(winner)
-
+    print(f"Winner:{winner}")
+    
     message= (
         f"Election Results\n"
         f".....................\n"
         f"Total Votes: {voter_id}\n"
         f".....................\n"
         f"{candidate_counts}\n"
-        f"{candidate}:{percentage}%\n"
-        f".....................\n"
+        f".....................\n")   
+        
+    
+    for candidate, count in candidate_counts.items():
+        percentage = round((count / voter_id) * 100, 3)
+        print(f"{candidate}: {percentage}%")
+        message+= (f"{candidate}:{percentage}%\n")
+    message += (f".....................\n"
         f"Winner: {winner}")
 
      #create new text file with analysis
-    with open("PyPoll.txt", "a") as f:
+    with open("PyPoll.txt", "w") as f:
         f.write(message)
-
-
-    
-
-        
 # %%
